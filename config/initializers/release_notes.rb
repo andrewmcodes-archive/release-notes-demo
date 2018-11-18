f6620d093e5a9eb7d57cf4e7098e57c82b4b388d
Andrew Mason <andrew.mason@n2pub.com>
2018-11-18 07:49:15 -0500

    RND #2 Initialize release-notes ;F

---
+++ config/initializers/release_notes.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+Release::Notes.configure do |config|
+  config.output_file = './RELEASE_NOTES.md'
+  config.temp_file = './release-notes.tmp.md'
+  config.include_merges = false
+  config.ignore_case = true
+  config.log_format = '- %s'
+  config.extended_regex = true
+  config.bug_labels = %w[;B Bug Error]
+  config.feature_labels = %w[;F ;E Add Create]
+  config.misc_labels = %w[;M Refactor]
+  config.bug_title = '**Fixed bugs:**'
+  config.feature_title = '**Implemented enhancements:**'
+  config.misc_title = '**Miscellaneous:**'
+  config.link_to_labels = %w["RND #", "AB #", "BC #"]
+  config.link_to_humanize = %w[ReleaseNotesDemo LabelAB LabelBC]
+  config.link_to_sites = %w[https:\/\/release-notes-demo\/projects https:\/\/label-ab\/projects https:\/\/label-bc\/projects]
+  config.timezone = 'America/New_York'
+end
